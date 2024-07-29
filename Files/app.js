e8510853da2beec2152c4cf751ec68a864bb9434
Hamza <153912992+MuhammadHamzaAslam@users.noreply.github.com>
2024-07-29 15:12:50 +0500

    Allhamdulliah All Done

--- Files/app.js
+++ Files/app.js
@@ -6,10 +6,15 @@ let checkAmount = document.getElementById('check-amount')
 let showBudget = document.getElementById('showBudget')
 let showExpenses = document.getElementById('showExpenses')
 let showBalance = document.getElementById('showBalance')
+let tbody = document.getElementById('tbody')
 let totalExpenses = 0
 setBudget.addEventListener('click',()=>{
     if (budgetInp.value === '') {
-        alert('Enter Your Budget')
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "You Didn't Enter Your Budget",
+          });
     }
     else{
         showBudget.innerText = budgetInp.value
@@ -24,6 +29,21 @@ checkAmount.addEventListener('click',()=>{
         let showBalanceValue = parseFloat(showBalance.innerText)
         showBalanceValue -= productCoastValue
         showBalance.innerText = showBalanceValue
+        let row = document.createElement('tr')
+        row.innerHTML += `
+            <tr>
+                <td>${productName.value}</td>
+                <td>${productCoastValue}</td>
+                <td><i class="fa-solid fa-trash"></i></td>
+            </tr>
+        `
+        tbody.appendChild(row)
+        row.querySelector('.fa-trash').addEventListener('click',()=>{
+            row.remove()
+            totalExpenses -= productCoastValue
+            showExpenses.innerText = totalExpenses
+            showBalance.innerText = parseFloat(showBalance.innerText) + productCoastValue
+        })
         productName.value = ""
         productCoast.value = ""
     }
